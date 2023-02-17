78e3d28f9137768863c8bf3280f0b59778595797
eliana osorio <yasminosorio2013@gmail.com>
2023-02-17 11:25:35 -0500

    fix:site design improvements

--- src/js/mentors.js
+++ src/js/mentors.js
@@ -30,8 +30,8 @@ function showLoadMoreButton() {
 function createDiv(user, count) {
   const div = document.createElement('div');
   div.setAttribute('id', `card-${count}`);
-  div.setAttribute('class', 'card col-sm-3');
-  div.style.display = 'inline-block';
+  div.setAttribute('class', 'card col-sm-3 cardh');
+  //div.style.display = 'inline-block';
   document.getElementsByTagName('body')[0].appendChild(div);
 
   const link = document.createElement('a');
@@ -86,7 +86,7 @@ function showErrorMessage(error) {
   div.setAttribute('id', 'error-card');
   div.setAttribute('class', 'card col-sm-2 alert alert-danger');
   div.style.width = '18rem';
-  div.style.display = 'inline-block';
+  //div.style.display = 'inline-block';
   div.textContent = `Sorry, we can't load mentors list, error: ${error}`;
 
   mentorsDiv.appendChild(div);
